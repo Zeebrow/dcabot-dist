c17fad4ba1d8353683284f8c3f91823b7ad8831a
Mike Zborowski <mzborowski@yahoo.com>
2021-05-08 19:17:35 -0400

    dcabot is now top-level package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dcabot-zeebrow",
+    name="dcabot",
     version="0.0.4",
     author="Zeebrow",
     author_email="zeebrow@dcabot-project.com",
